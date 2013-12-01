0aa554437283c4d2cafa6ed97d5f12e730ea0d5d
Emanual Jade <emanualjade@gmail.com>
2013-12-01 15:58:28 -0800

    Initial commit with mobile utility function

---
+++ ej.js
@@ -0,0 +1,64 @@
+var ej = {
+  
+  /*
+    ej.isMobile() true || false
+
+    ej.isMobile(function(match, device){
+      //match is true or false
+      //device is false or device matched as a string
+      console.log(match + " " + device);
+    }); true || false
+
+    ej.isMobile('ipad, android, iphone', function(match, device){
+      // you can pass in multiple params to match against
+      // this case will check for a match against ipad || android || iphone
+      // match is true or false
+      // device is false or device matched as a string
+      console.log("string match: " + match + " " + device);
+    });
+  */
+
+  isMobile: function(device, callback){
+    var userAgent = navigator.userAgent||navigator.vendor||window.opera;
+    var deviceArray = [];
+    var callbackfunction = false;
+    var match = false;
+    var deviceName = false;
+    
+    for(var i=0; i < arguments.length; i++){
+      if(typeof arguments[i] == "string"){
+        var deviceArray = arguments[i].split(",");
+      }
+      if(typeof arguments[i] == 'function'){
+        callbackfunction = true;
+        callback = arguments[i];
+      }      
+    }
+    
+    if(deviceArray.length > 0){
+      // if string search param passed match against supplied devices
+      for(var i=0; i < deviceArray.length; i++){
+        var re = new RegExp(deviceArray[i].trim(), "i");
+        if( userAgent.match(re) ){
+          deviceName = navigator.userAgent.match(re)[0];
+          match = true;
+          break;
+        }
+      }   
+    }else{
+      // if no string search param passed do a match against mobile devices
+      var mobileRegex = /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|ad|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|webOS |windows (ce|phone)|xda|xiino|android|playbook|tablet/i;
+      if( userAgent.match(mobileRegex) ){
+        deviceName = userAgent.match(mobileRegex)[0];
+        match = true;
+      }
+    }
+
+    if(callbackfunction){
+      callback(match, deviceName);
+    }
+    return match;
+
+  }
+
+};
